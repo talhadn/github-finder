a34ad6e5b527384c220c856795de2489ae625a77
Talha Sehgal <talhadn@hotmail.com>
2022-08-18 20:11:00 +0100

    basic UI

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
+import Spinner from '../layout/Spinner'
+import UserItem from './UserItem'
+
+function UserResults() {
+    const {users,loading,fetchUsers} = useContext(GithubContext)
+
+    useEffect(()=>{fetchUsers()},[])
+
+
+    if (!loading) {
+        return (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+                {users.map((user) => (
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+        )
+    } else {
+        return <Spinner />
+    }
+}
+
+export default UserResults
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UserResults from '../components/users/UserResults'
+
+function home() {
+  return (
+    <>
+        <UserResults />
+    </>
+  )
+}
+
+export default home
